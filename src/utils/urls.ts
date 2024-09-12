75f6215b8b439e033144bc891a27761b0453788f
unknown <aronilie.code@gmail.com>
2024-09-12 08:17:07 +0300

    Updated src path of image

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,7 +1,7 @@
 const prodApiUrl = 'https://api.autotrafic.es';
 const localApiUrl = 'http://localhost:3100';
 
-export const BASE_API_URL = prodApiUrl;
+export const BASE_API_URL = localApiUrl;
 
 export const WHATSAPP_HELP_LINK =
   'https://api.whatsapp.com/send?phone=34643219297&text=Hola%2C%20se%20ha%20producido%20un%20error%20cuando%20intentaba%20subir%20los%20documentos%20del%20veh%C3%ADculo%2C%20%C2%BFpodr%C3%ADas%20ayudarme%3F';

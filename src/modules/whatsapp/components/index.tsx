6ff108b08762a393c6de2b761b3b9e45592a808e
unknown <aronilie.code@gmail.com>
2025-02-13 20:47:49 +0100

    WhatsApp: Added date separator

--- src/modules/whatsapp/components/index.tsx
+++ src/modules/whatsapp/components/index.tsx
@@ -126,28 +126,28 @@ export function Whatsapp() {
 
   const sendMessage = async () => {
     if (!newMessage.trim()) return;
-  
+
     try {
       setLoadingSendMessage(true);
-  
+
       // Check if the selected chat is a new chat (i.e., not in the original chats list)
       const isNewChat = !chats.some((chat) => chat.id === selectedChat?.id);
-  
+
       // Send the message with the correctly formatted chatId
       await axios.post(`${WHATSAPP_API_URL}/messages/send`, {
         message: newMessage,
         phoneNumber: selectedChat?.id?.replace('@c.us', ''),
       });
-  
+
       setNewMessage('');
-  
+
       // If it's a new chat, add it to the chats list
       if (isNewChat && selectedChat) {
         const updatedChats = [selectedChat, ...chats];
         setChats(updatedChats);
         setFilteredChats([selectedChat]); // Ensure the new chat remains in the filtered list
       }
-  
+
       // Fetch the updated messages for the selected chat
       const updatedMessages = await axios.get(`${WHATSAPP_API_URL}/messages/chat-messages/${selectedChat?.id}`);
       setMessages((prev) => [
@@ -164,17 +164,17 @@ export function Whatsapp() {
   const normalizeNumber = (number: string) => {
     // Remove all non-numeric characters
     const cleanedNumber = number.replace(/\D/g, '');
-  
+
     // If the number starts with '+', treat it as a foreign number
     if (number.startsWith('+')) {
       return cleanedNumber; // Return the number with the country code
     }
-  
+
     // If the number is a Spanish default number (e.g., 674218987), add the country code '34'
     if (cleanedNumber.length === 9 && !cleanedNumber.startsWith('34')) {
       return `34${cleanedNumber}`; // Add Spain's country code
     }
-  
+
     // If the number already starts with '34', assume it's a Spanish number
     return cleanedNumber;
   };
@@ -192,7 +192,7 @@ export function Whatsapp() {
     const escapedSearchQuery = escapeRegExp(searchQuery); // Escape special characters
     const searchRegex = new RegExp(escapedSearchQuery.replace(/\s+/g, '.*'), 'i');
     const normalizedSearchQuery = normalizeNumber(searchQuery);
-  
+
     // Filter chats based on the search query
     const filtered = chats.filter((chat) => {
       const normalizedChatId = normalizeNumber(chat.id);
@@ -201,7 +201,7 @@ export function Whatsapp() {
       const matchesMessages = messages.some((msg) => msg.chatId === chat.id && searchRegex.test(msg.body));
       return matchesChatName || matchesChatId || matchesMessages;
     });
-  
+
     // If no matches are found and the search query is a valid number, add the new chat
     if (filtered.length === 0 && normalizedSearchQuery) {
       const newChat: WChat = {
@@ -212,7 +212,7 @@ export function Whatsapp() {
         timestamp: Date.now(),
         lastMessage: { viewed: false, body: '' },
       };
-  
+
       // Set filteredChats to only contain the new chat
       setFilteredChats([newChat]);
     } else {
@@ -224,27 +224,43 @@ export function Whatsapp() {
   const selectChat = (chat: WChat) => {
     setSelectedChat(chat);
     setSearchQuery(''); // Clear the search query
-  
+
     // Check if the chat is a new chat (i.e., not in the original chats list)
     const isNewChat = !chats.some((c) => c.id === chat.id);
-  
+
     // If it's a new chat, add it to the chats list
     if (isNewChat) {
       const updatedChats = [chat, ...chats];
       setChats(updatedChats);
       setFilteredChats(updatedChats); // Reset filteredChats to include all chats
     }
-  
+
     setMessages([]);
     setLoadingMessages(true);
-  
+
     // Send the correctly formatted chatId to the backend
     axios.get(`${WHATSAPP_API_URL}/messages/seen-chat/${chat.id}`);
-  
+
     axios.get(`${WHATSAPP_API_URL}/messages/chat-messages/${chat.id}`).then((messageResponse) => {
       setMessages(messageResponse.data.messages.map((msg: WMessage) => ({ ...msg, chatId: chat.id })));
       setLoadingMessages(false);
     });
+  };
+
+  const formatDate = (timestamp: number) => {
+    return new Date(timestamp * 1000).toLocaleDateString('es-ES', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  };
+
+  // Helper function to determine if a date separator should be rendered
+  const shouldRenderDateSeparator = (currentTimestamp: number, previousTimestamp: number | null) => {
+    if (!previousTimestamp) return true; // Always render for the first message
+    const currentDate = formatDate(currentTimestamp);
+    const previousDate = formatDate(previousTimestamp);
+    return currentDate !== previousDate;
   };
 
   useEffect(() => {
@@ -306,48 +322,68 @@ export function Whatsapp() {
           {selectedChat &&
             messages
               .filter((msg) => msg.chatId === selectedChat.id)
-              .map((message) => (
-                <div key={message.id} className={`flex ${message.fromMe ? 'justify-end' : 'justify-start'} my-2 mx-[10%]`}>
-                  <div
-                    className={`px-2 py-1 text-sm rounded-xl ${
-                      message.fromMe ? 'bg-green-100' : 'bg-white'
-                    } max-w-lg shadow flex gap-4 items-end`}
-                    style={{ minHeight: '32px' }}
-                  >
-                    {message.hasMedia ? (
-                      message.mimetype?.startsWith('image/') ? (
-                        <div className="flex flex-col gap-2">
-                          <img src={message.mediaUrl} alt="media" className="max-w-[400px] max-h-[400px] rounded-lg" />
-                          <p>{message.body}</p>
+              .map((message, index, filteredMessages) => {
+                const currentTimestamp = message.timestamp;
+                const previousTimestamp = index > 0 ? filteredMessages[index - 1].timestamp : null;
+
+                // Check if a date separator should be rendered
+                const renderDateSeparator = shouldRenderDateSeparator(currentTimestamp, previousTimestamp);
+
+                return (
+                  <div key={message.id}>
+                    {/* Render the date separator */}
+                    {renderDateSeparator && (
+                      <div className="flex justify-center my-4">
+                        <div className="px-3 py-1 bg-gray-300 text-gray-700 rounded-full text-sm">
+                          {formatDate(currentTimestamp)}
                         </div>
-                      ) : (
-                        <div className="flex flex-col gap-2">
-                          <a
-                            href={message.mediaUrl}
-                            download={`file.${message.mimetype?.split('/')[1]}`}
-                            className="p-2 bg-blue-500 text-white rounded-lg text-xs"
-                          >
-                            Descargar archivo
-                          </a>
-                          <p>{message.body}</p>
-                        </div>
-                      )
-                    ) : (
-                      <p dangerouslySetInnerHTML={{ __html: message.body.replace(/\n/g, '<br />') }} />
+                      </div>
                     )}
-                    <div className="text-xs text-gray-500 bottom-1 right-2 flex items-center">
-                      {new Date(message.timestamp * 1000).toLocaleTimeString(undefined, {
-                        hour: '2-digit',
-                        minute: '2-digit',
-                        hour12: false,
-                      })}
-                      {message.fromMe && (
-                        <span className="ml-1">{message.viewed ? <DoubleBlueCheckIcon /> : <DoubleCheckIcon />}</span>
-                      )}
+
+                    {/* Render the message */}
+                    <div className={`flex ${message.fromMe ? 'justify-end' : 'justify-start'} my-2 mx-[10%]`}>
+                      <div
+                        className={`px-2 py-1 text-sm rounded-xl ${
+                          message.fromMe ? 'bg-green-100' : 'bg-white'
+                        } max-w-lg shadow flex gap-4 items-end`}
+                        style={{ minHeight: '32px' }}
+                      >
+                        {message.hasMedia ? (
+                          message.mimetype?.startsWith('image/') ? (
+                            <div className="flex flex-col gap-2">
+                              <img src={message.mediaUrl} alt="media" className="max-w-[400px] max-h-[400px] rounded-lg" />
+                              <p>{message.body}</p>
+                            </div>
+                          ) : (
+                            <div className="flex flex-col gap-2">
+                              <a
+                                href={message.mediaUrl}
+                                download={`file.${message.mimetype?.split('/')[1]}`}
+                                className="p-2 bg-blue-500 text-white rounded-lg text-xs"
+                              >
+                                Descargar archivo
+                              </a>
+                              <p>{message.body}</p>
+                            </div>
+                          )
+                        ) : (
+                          <p dangerouslySetInnerHTML={{ __html: message.body.replace(/\n/g, '<br />') }} />
+                        )}
+                        <div className="text-xs text-gray-500 bottom-1 right-2 flex items-center">
+                          {new Date(message.timestamp * 1000).toLocaleTimeString(undefined, {
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            hour12: false,
+                          })}
+                          {message.fromMe && (
+                            <span className="ml-1">{message.viewed ? <DoubleBlueCheckIcon /> : <DoubleCheckIcon />}</span>
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
         </div>
 
         {selectedChat && (
